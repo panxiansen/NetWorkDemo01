72a0cacb05b84cfbb918365e12f860b99e536d2b
Tonsom <a707718856>
2016-08-03 23:22:31 +0800

    接口数据调用测试
    聚合天气数据接口调用，进行测试，解析，试用Key是否能获取数据

--- src/com/example/networkdemo01/MainActivity.java
+++ src/com/example/networkdemo01/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.networkdemo01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -42,6 +55,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		sendRequest = (Button)findViewById(R.id.send_request);
+		textResponse = (TextView)findViewById(R.id.text_response);
 	}
 
 	@Override
@@ -55,37 +69,92 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		if(v.getId() == R.id.send_request){
-			sendRequestWithHttpClient();
+			sendRequestWithHttpUrlConnection();
 		}
 	}
 
-	private void sendRequestWithHttpClient() {
+	private void sendRequestWithHttpUrlConnection() {
 		// TODO Auto-generated method stub
 		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				try {
-					HttpClient httpClient = new DefaultHttpClient();
-					HttpGet httpGet = new HttpGet("http://v.juhe.cn/weather/citys?dtype=&key=283a0a5758e7077da40b4fea4860a6e6");
-					HttpResponse httpResponse = httpClient.execute(httpGet);
-					if(httpResponse.getStatusLine().getStatusCode() == 200){
-						//响应成功了,获取网络文本实体
-						HttpEntity entity = httpResponse.getEntity();
-						String response = EntityUtils.toString(entity,"utf-8");
-						response.split(",");
-						Message message = new Message ();
-						message.what = SHOW_RESPONSE;
-						handler.sendMessage(message);
-					}
-					
-					
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
+				URL url;  
+                HttpURLConnection connection = null;  
+                try {  
+                    String cityName = URLEncoder.encode("广州", "utf-8");  
+                    url = new URL(  
+                            "http://v.juhe.cn/weather/index?format=2&cityname="  
+                                    + cityName  
+                                    + "&key=283a0a5758e7077da40b4fea4860a6e6");  
+                    connection = (HttpURLConnection) url.openConnection();  
+                    connection.setRequestMethod("GET");  
+                    connection.setConnectTimeout(8000);  
+                    connection.setReadTimeout(8000);  
+                    InputStream in = connection.getInputStream();  
+                    // 下面对获取到的输入流进行读取  
+                    BufferedReader reader = new BufferedReader(  
+                            new InputStreamReader(in));  
+                    StringBuilder response = new StringBuilder();  
+                    String line;  
+                    while ((line = reader.readLine()) != null) {  
+                        response.append(line);  
+                    }  
+                    System.out.println("response=" + response.toString());  
+                    //parseWithJSON(response.toString());  
+                    parseWeatherWithJSON(response.toString());  
+                    parseWithJSON(response.toString());
+                    Message message = new Message();  
+                    message.what = SHOW_RESPONSE;  
+                    // 将服务器返回的结果存放到Message中  
+                    message.obj = response.toString();  
+                    handler.sendMessage(message);  
+                } catch (MalformedURLException e) {  
+                    e.printStackTrace();  
+                } catch (IOException e) {  
+                    e.printStackTrace();  
+                } finally {  
+                    if (connection != null) {  
+                        connection.disconnect();  
+                    }  
+                }  
 			}
-		}).start();
-	}
-
+        }).start();
+  
+    }  
+  
+    protected void parseWeatherWithJSON(String response) {  
+        try {  
+            JSONObject jsonObject=new JSONObject(response);  
+            String resultcode=jsonObject.getString("resultcode");  
+            if(resultcode.equals("200")){  
+                JSONObject resultObject=jsonObject.getJSONObject("result");  
+                JSONObject todayObject=resultObject.getJSONObject("today");  
+                String date_y=todayObject.getString("date_y");  
+                String week=todayObject.getString("week");  
+                String temperature=todayObject.getString("temperature");  
+                Log.d("MainActivity", "date_y="+date_y+"week="+week+"temp="+temperature);  
+            }  
+              
+        } catch (JSONException e) {  
+            e.printStackTrace();  
+        }  
+    }  
+  
+    private  void parseWithJSON(String response) {  
+        try {  
+            JSONArray jsonArray = new JSONArray(response);  
+            for (int i = 0; i < jsonArray.length(); i++) {  
+                JSONObject jsonObject = jsonArray.getJSONObject(i);  
+                String id = jsonObject.getString("id");  
+                String name = jsonObject.getString("name");  
+                String version = jsonObject.getString("version");  
+                Log.d("MainActivity", "id=" + id + "name=" + name + "version="  
+                        + version);  
+            }  
+        } catch (JSONException e) {  
+            e.printStackTrace();  
+        }  
+    }  
 }
